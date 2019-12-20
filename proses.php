f6875feebe0cfbad895219d1d460e88140c7fc94
hdpolover <hendrapolover@gmail.com>
2019-12-20 15:06:38 +0700

    updated

---
+++ proses.php
@@ -0,0 +1,31 @@
+<?php
+//validasi
+if (trim($_POST['nim']) == '') {
+    $error[] = '- NIM harus diisi';
+}
+if (trim($_POST['nama']) == '') {
+    $error[] = '- Nama harus diisi';
+}
+if (trim($_POST['tempat_lahir']) == '') {
+    $error[] = '- Tempat Lahir harus diisi';
+}
+//dan seterusnya
+
+if (isset($error)) {
+    echo '<b>Error</b>: <br />'.implode('<br />', $error);
+} else {
+    /*
+    jika data mau dimasukkan ke database,
+    maka perintah SQL INSERT bisa ditulis di sini
+    */
+
+    $data = '';
+    foreach ($_POST as $k => $v) {
+        $data .= "$k : $v<br />";
+    }
+
+    echo '<b>Form berhasil disubmit. Berikut ini data anda:</b><br />';
+    echo $data;
+}
+die();
+?> 
